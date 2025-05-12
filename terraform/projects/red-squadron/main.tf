135dfc8c77d7807e0c9675b70ddf169a11a2650f
William Taylor <me@willtaylor.info>
2025-05-12 21:47:18 +0100

    add terraform

---
+++ terraform/projects/red-squadron/main.tf
@@ -0,0 +1,49 @@
+locals {
+  talos_version = "v1.10.1"
+}
+
+data "talos_image_factory_extensions_versions" "red_squadron" {
+  talos_version = local.talos_version
+  filters = {
+    names = [
+      "cloudflared",
+      "gasket-driver",
+      "i915",
+      "intel-ice-firmware",
+      "intel-ucode",
+      "iscsi-tools",
+      "mei",
+      "nut-client",
+      "qemu-guest-agent",
+      "thunderbolt",
+      "tailscale",
+    ]
+  }
+}
+
+resource "talos_image_factory_schematic" "red_squadron" {
+  schematic = yamlencode(
+    {
+      customization = {
+        systemExtensions = {
+          officialExtensions = data.talos_image_factory_extensions_versions.red_squadron.extensions_info.*.name
+        }
+      }
+    }
+  )
+}
+
+data "talos_image_factory_urls" "red_squadron" {
+  talos_version = local.talos_version
+  schematic_id  = talos_image_factory_schematic.red_squadron.id
+  architecture  = "amd64"
+  platform      = "nocloud"
+}
+
+resource "proxmox_virtual_environment_download_file" "talos_cloud_image" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "red-one"
+
+  url = data.talos_image_factory_urls.red_squadron.urls.iso
+}
---
+++ terraform/projects/red-squadron/providers.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 1.11.4"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.77.1"
+    }
+    talos = {
+      source  = "siderolabs/talos"
+      version = "0.8.0"
+    }
+  }
+  backend "s3" {
+    endpoints = {
+      s3 = "https://s3.eu-central-003.backblazeb2.com"
+    }
+    region = "main"
+    bucket = "0NIwoqKe3wi8z9fm0RrN5bm5L7tfstate"
+    key    = "terraform/device-config/red-squadron/terraform.tfstate"
+
+    skip_requesting_account_id  = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true
+  }
+}
+
+provider "talos" {}
+
+provider "proxmox" {
+  endpoint = "https://red-one.willtaylor.info:8006"
+  insecure = true
+
+  ssh {
+    agent = true
+  }
+}
+
