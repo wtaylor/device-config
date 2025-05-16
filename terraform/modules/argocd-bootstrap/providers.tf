ca830d6a56cd0b657b397ae72c96086462304d51
William Taylor <me@willtaylor.info>
2025-05-16 20:09:43 +0100

    remove argocd core

--- terraform/modules/argocd-bootstrap/providers.tf
+++ terraform/modules/argocd-bootstrap/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.36.0"
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.19.0"
     }
   }
 }
